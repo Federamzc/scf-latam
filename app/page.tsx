2d74ac8141e498689e8d75b0ba680ebb8e8be4a7
Federico Ramirez <federamirez@MacBookAir.lan>
2026-04-15 16:58:48 -0600

    fix: landing redirect correcto

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
-import { redirect } from 'next/navigation'
-
 export default function Home() {
-  redirect('/landing.html')
+  return (
+    <script dangerouslySetInnerHTML={{
+      __html: `window.location.replace('/index.html')`
+    }} />
+  )
 }
