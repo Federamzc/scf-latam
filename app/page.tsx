6937115497185af358ce4b0dea8216b392a85151
Federico Ramirez <federamirez@MacBookAir.lan>
2026-04-17 14:59:29 -0600

    fix: landing redirect sin iframe

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
+'use client'
+import { useEffect } from 'react'
+
 export default function Home() {
-  return (
-    <iframe
-      src="/index.html"
-      style={{ width: '100%', height: '100vh', border: 'none' }}
-    />
-  )
+  useEffect(() => {
+    window.location.replace('/index.html')
+  }, [])
+  return null
 }
